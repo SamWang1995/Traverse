811dbf348be9c616711ae581b936242fa19879e1
SamWang1995 <Sam@Samuels-MacBook-Air.local>
2015-09-15 22:06:58 -0700

    Initial Commit
    Initial Commit

---
+++ Test1/Test1/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Test1
+//
+//  Created by Samuel Wang on 9/14/15.
+//  Copyright (c) 2015 Samuel Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var hello: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
